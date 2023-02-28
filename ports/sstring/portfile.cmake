70c483547d6b0aabeedd57b452d8244e2983d5de
Kaoru <SHIINA_KAORU@outlook.com>
2023-02-28 18:00:54 +0800

    update ports/sstring

--- ports/sstring/portfile.cmake
+++ ports/sstring/portfile.cmake
@@ -1,9 +1,9 @@
 set(SOURCE_PATH ${CURRENT_BUILDTRESS_DIR}/sstring)
 
 vcpkg_download_distfile(ARCHIVE
-    URLS "https://github.com/libsese/SString/archive/refs/tags/v0.1.3.zip"
-    FILENAME "SString-0.1.3.zip"
-    SHA512 c7bdda20f3c25276941eacd445d77908c541a0150aa85c39fef64585debb086c71817b0fe7438f2b0844f769d595c08a0e34065bbd5b21582c8c9778ec7f5863
+    URLS "https://github.com/libsese/SString/archive/refs/tags/v0.1.4.zip"
+    FILENAME "SString-0.1.4.zip"
+    SHA512 a869c46ee721e277710f245ced9f6f959b36a1d05ad7edd35cc923aec3d65ef9e6f7626d7059ca26d448af4f4ee1d63706076ca5947285875d4d9f403f6bec77
 )
 
 vcpkg_extract_source_archive_ex(
