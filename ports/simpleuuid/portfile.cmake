a60cea4580ee48967083b9b7a8b8b8c89a05996e
Kaoru <SHIINA_KAORU@outlook.com>
2023-03-21 12:29:11 +0800

    Code formatting

--- ports/simpleuuid/portfile.cmake
+++ ports/simpleuuid/portfile.cmake
@@ -1,18 +1,18 @@
 set(SOURCE_PATH ${CURRENT_BUILDTRESS_DIR}/simpleuuid)
 
 vcpkg_download_distfile(ARCHIVE
-    URLS "https://github.com/libsese/SimpleUuid/archive/refs/tags/v0.1.4.zip"
-    FILENAME "SimpleUuid-0.1.4.zip"
-    SHA512 9f2272bbc7121051fa4477b36c3841b2ff3fbdd0e4be2fcb5a1ec153510667b9e516bb5197f8297da35c8a6a4c99b848732d6895cc36d02553596d6b54271423
-)
+        URLS "https://github.com/libsese/SimpleUuid/archive/refs/tags/v0.1.4.zip"
+        FILENAME "SimpleUuid-0.1.4.zip"
+        SHA512 9f2272bbc7121051fa4477b36c3841b2ff3fbdd0e4be2fcb5a1ec153510667b9e516bb5197f8297da35c8a6a4c99b848732d6895cc36d02553596d6b54271423
+        )
 
 vcpkg_extract_source_archive_ex(
-    OUT_SOURCE_PATH SOURCE_PATH
-    ARCHIVE "${ARCHIVE}"
+        OUT_SOURCE_PATH SOURCE_PATH
+        ARCHIVE "${ARCHIVE}"
 )
 
 vcpkg_cmake_configure(
-    SOURCE_PATH "${SOURCE_PATH}"
+        SOURCE_PATH "${SOURCE_PATH}"
 )
 
 vcpkg_cmake_install()
--- ports/sstring/portfile.cmake
+++ ports/sstring/portfile.cmake
@@ -1,18 +1,18 @@
 set(SOURCE_PATH ${CURRENT_BUILDTRESS_DIR}/sstring)
 
 vcpkg_download_distfile(ARCHIVE
-    URLS "https://github.com/libsese/SString/archive/refs/tags/v0.1.4.zip"
-    FILENAME "SString-0.1.4.zip"
-    SHA512 a869c46ee721e277710f245ced9f6f959b36a1d05ad7edd35cc923aec3d65ef9e6f7626d7059ca26d448af4f4ee1d63706076ca5947285875d4d9f403f6bec77
-)
+        URLS "https://github.com/libsese/SString/archive/refs/tags/v0.1.4.zip"
+        FILENAME "SString-0.1.4.zip"
+        SHA512 a869c46ee721e277710f245ced9f6f959b36a1d05ad7edd35cc923aec3d65ef9e6f7626d7059ca26d448af4f4ee1d63706076ca5947285875d4d9f403f6bec77
+        )
 
 vcpkg_extract_source_archive_ex(
-    OUT_SOURCE_PATH SOURCE_PATH
-    ARCHIVE "${ARCHIVE}"
+        OUT_SOURCE_PATH SOURCE_PATH
+        ARCHIVE "${ARCHIVE}"
 )
 
 vcpkg_cmake_configure(
-    SOURCE_PATH "${SOURCE_PATH}"
+        SOURCE_PATH "${SOURCE_PATH}"
 )
 
 vcpkg_cmake_install()
