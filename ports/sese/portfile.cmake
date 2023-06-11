62863efc5397dd3770d5625dc8798faa941af085
Kaoru <SHIINA_KAORU@outlook.com>
2023-06-11 23:56:31 +0800

    update: update ports/sese version

--- ports/sese/portfile.cmake
+++ ports/sese/portfile.cmake
@@ -1,14 +1,20 @@
 set(SOURCE_PATH ${CURRENT_BUILDTRESS_DIR}/sese)
 
 vcpkg_download_distfile(ARCHIVE
-        URLS "https://github.com/libsese/sese-core/archive/refs/tags/0.5.3a.zip"
-        FILENAME "sese-core-0.5.3a.zip"
-        SHA512 b7f40babaed592bab899d54e78e555817e3d0b037ac2da1978ba70df32d44cb77c219097c85599097044d19bd2fdf1a3889fd2aa147c565623840807d59bf611
+        URLS "https://github.com/libsese/sese-core/archive/refs/tags/0.6.3.zip"
+        FILENAME "sese-core-0.6.3.zip"
+        SHA512 b13266fbac649220518cd2f13428326bbe5b2b8c41104ed2fb7c9be1690f0944f4ba7910b6a5da4f5a7be849347bfb5445d4b2a904b907751a0fddee993777ea
         )
 
 vcpkg_extract_source_archive_ex(
         OUT_SOURCE_PATH SOURCE_PATH
         ARCHIVE "${ARCHIVE}"
+)
+
+vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+        FEATURES
+        tests        SESE_BUILD_TEST
+        async-logger USE_ASYNC_LOGGER
 )
 
 vcpkg_cmake_configure(
