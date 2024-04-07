0a72c36db29b1d5bbbc74d9722e3d0c569a48b09
kaoru <shiina_kaoru@outlook.com>
2024-04-07 21:53:29 +0800

    update: ports/sese-core 2.0.1

--- ports/sese-core/portfile.cmake
+++ ports/sese-core/portfile.cmake
@@ -3,8 +3,8 @@ set(SOURCE_PATH ${CURRENT_BUILDTRESS_DIR}/sese)
 vcpkg_from_github(
         OUT_SOURCE_PATH SOURCE_PATH
         REPO libsese/sese-core
-        REF 2.0.0
-        SHA512 5ca1e212c99fdc8807d8622a1c728b3280ffa18d58b5a46bbd6e65eb0df2e8f92cd8a33cbeeeb02e437f2666d74d3631cd58424a2b9bd2a65db2c08b917036fc
+        REF 2.0.1
+        SHA512 9500eae8de27a1a36c4423f388434bf78759781a67221fb5e45bece8975d14a6d3d0987d98cd6b73d90fdd8c1218894691c26093d8044a2bb451e4b4326f2e1c
 )
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
@@ -13,7 +13,8 @@ vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
         mysql        SESE_DB_USE_MARIADB
         sqlite3      SESE_DB_USE_SQLITE
         psql         SESE_DB_USE_POSTGRES
-        async-logger USE_ASYNC_LOGGER
+        async-logger SESE_USE_ASYNC_LOGGER
+        archive      SESE_USE_ARCHIVE
 )
 
 vcpkg_cmake_configure(
@@ -29,3 +30,5 @@ file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
 file(COPY "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/sese-core")
 file(RENAME "${CURRENT_PACKAGES_DIR}/share/sese-core/LICENSE" "${CURRENT_PACKAGES_DIR}/share/sese-core/copyright")
+
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
